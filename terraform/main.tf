76a74ec7674e52c86d701be57f8ec0f71542b10a
Naresh Chandanbatve <chandanbatven@gmail.com>
2024-06-19 19:38:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test-instance" {
+resource "aws_instance" "test-instance" { 
   ami           = "ami-0e1d06225679bc1c5"  # amazon linux
   instance_type = "t2.micro"
   # key_name      = "test"  # SSH key pair
